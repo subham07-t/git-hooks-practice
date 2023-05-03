ff7dc958f62d1e2b177490e0974a18c129360492
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-03 21:52:33 +0530

    husky test

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, "always", ["FE:feat", "FE:fix"]],
-    "type-case": [2, "always", "upper-case"],
-    "type-empty": [2, "never"],
-  },
-};
