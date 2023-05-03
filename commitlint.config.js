ffe9b019c5016c6cdfaecbb44e66e163b8c4af03
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-03 22:49:51 +0530

    feat(fe): test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  // extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-length": [1, "always", 600],
+    "scope-empty": [2, "always"],
+    "scope-enum": [2, "always", ["fe", "be"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-empty": [2, "never"],
+    "type-enum": [2, "always"],
+  },
+};
